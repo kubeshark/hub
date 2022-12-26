7eaa9c6d5cbde85d1bb87f6f361a9af5c24edac3
M. Mert Yildiran <me@mertyildiran.com>
2022-12-26 05:00:41 +0300

    :hammer: Add `server` package

--- main.go
+++ main.go
@@ -2,24 +2,19 @@ package main
 
 import (
 	"flag"
-	"net/http"
 	"os"
 	"os/signal"
 	"strings"
 	"time"
 
-	"github.com/gin-gonic/gin"
 	"github.com/kubeshark/hub/pkg/config"
-	"github.com/kubeshark/hub/pkg/middlewares"
 	"github.com/kubeshark/hub/pkg/misc"
-	"github.com/kubeshark/hub/pkg/routes"
-	"github.com/kubeshark/hub/pkg/utils"
 	"github.com/kubeshark/hub/pkg/worker"
+	"github.com/kubeshark/hub/server"
 	"github.com/rs/zerolog"
 	"github.com/rs/zerolog/log"
 )
 
-var namespace = flag.String("namespace", "", "Resolve IPs if they belong to resources in this namespace (default is all)")
 var port = flag.Int("port", 80, "Port number of the HTTP server")
 var debug = flag.Bool("debug", false, "Enable debug mode")
 var workerHostsFlag = flag.String("worker-hosts", worker.HostWithPort(worker.DefaultWorkerHost), "hostname:port pairs of worker instances to access their WebSocket and HTTP endpoints")
@@ -40,42 +35,17 @@ func main() {
 
 	log.Info().Msg("Initializing the Hub...")
 
-	ginApp := runInApiServerMode(*namespace)
+	if err := config.LoadConfig(); err != nil {
+		log.Fatal().Err(err).Msg("While loading the config file!")
+	}
 
-	utils.StartServer(ginApp, *port)
+	ginApp := server.Build()
+
+	server.Start(ginApp, *port)
 
 	signalChan := make(chan os.Signal, 1)
 	signal.Notify(signalChan, os.Interrupt)
 	<-signalChan
 
 	log.Info().Msg("Exiting")
-}
-
-func hostApi() *gin.Engine {
-	ginApp := gin.New()
-	ginApp.Use(middlewares.DefaultStructuredLogger())
-	ginApp.Use(gin.Recovery())
-
-	ginApp.GET("/echo", func(c *gin.Context) {
-		c.String(http.StatusOK, "It's running.")
-	})
-
-	ginApp.Use(middlewares.CORSMiddleware())
-
-	routes.QueryRoutes(ginApp)
-	routes.ItemRoutes(ginApp)
-	routes.WebSocketRoutes(ginApp)
-	routes.MetadataRoutes(ginApp)
-	routes.PodsRoutes(ginApp)
-	routes.PcapsRoutes(ginApp)
-
-	return ginApp
-}
-
-func runInApiServerMode(namespace string) *gin.Engine {
-	if err := config.LoadConfig(); err != nil {
-		log.Fatal().Err(err).Msg("While loading the config file!")
-	}
-
-	return hostApi()
 }
--- pkg/utils/utils.go
+++ pkg/utils/utils.go
@@ -1,54 +1,11 @@
 package utils
 
 import (
-	"context"
 	"encoding/json"
-	"fmt"
-	"net/http"
 	"os"
-	"os/signal"
-	"syscall"
-	"time"
 
-	"github.com/gin-gonic/gin"
 	"github.com/rs/zerolog/log"
 )
-
-var (
-	StartTime int64 // global
-)
-
-// StartServer starts the server with a graceful shutdown
-func StartServer(app *gin.Engine, port int) {
-	signals := make(chan os.Signal, 2)
-	signal.Notify(signals,
-		os.Interrupt,    // this catch ctrl + c
-		syscall.SIGTSTP, // this catch ctrl + z
-	)
-
-	srv := &http.Server{
-		Addr:    ":8080",
-		Handler: app,
-	}
-
-	go func() {
-		<-signals
-		log.Warn().Msg("Shutting down...")
-		ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
-		defer cancel()
-		err := srv.Shutdown(ctx)
-		if err != nil {
-			log.Error().Err(err).Send()
-		}
-		os.Exit(0)
-	}()
-
-	// Run server.
-	log.Info().Msg("Starting the server...")
-	if err := app.Run(fmt.Sprintf(":%d", port)); err != nil {
-		log.Error().Err(err).Msg("Server is not running!")
-	}
-}
 
 func CheckErr(err error) {
 	if err != nil {
